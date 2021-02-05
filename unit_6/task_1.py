7529d920be6cb62f15a77309c227c81213dbee4b
Tania <rudenkotania@gmail.com>
2021-02-05 14:54:06 +0300

    added all tasks and rename unit_1 to task_1

--- unit_6/task_1.py
+++ unit_6/task_1.py
@@ -9,17 +9,17 @@ class TrafficLight:
         for i in range(1, 10000, 1):
 
             if (i - 1) % 3 == 0:
-                print(f"red")
-                time.sleep(1)
+                print(TrafficLight.__color)
+                time.sleep(7)
 
             if (i + 1) % 3 == 0:
                 print(f"yellow")
                 time.sleep(2)
 
             if i % 3 == 0:
-                time.sleep(2)
-                print(f"green")
 
+                print(f"green")
+                time.sleep(10)
 
 a = TrafficLight()
 a.running()
